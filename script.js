ffa1bc3e1d63212c9060f50ed2bf3773aca76b22
Shpat Spahiu <137379379+shpatspahiu@users.noreply.github.com>
2023-08-08 19:04:35 +0200

    Fix a bracket typo, develop game logic
    Fix a bracket typo (everything was inside the getComputerChoice fun.
    create game logic, playRound function checks the winner based on choice),
    and create win or loose dynamic message

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-const ROCK = 0;
-const PAPER = 1;
-const SCISSOR = 2;
-
 function getComputerChoice() {
   let max = 2;
   let min = 0;
   switch (Math.floor(Math.random() * (max - min + 1) + min)) {
     case 0:
-      return "GUR";
+      return "rock";
       break;
 
     case 1:
-      return "LETER";
+      return "paper";
       break;
 
     case 2:
-      return "GERSHERE";
+      return "scissors";
       break;
   }
+}
 
-  function playerSelection() {
-    let input = prompt("Enter Rock-Paper-Scissor");
-    input = input.trim().toUpperCase();
-    console.log(input);
+function getPlayerChoice() {
+  let input = prompt("Enter Rock-Paper-Scissor");
+  input = input.trim().toUpperCase();
+  console.log(input);
+  return input;
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+const playerWins = "You win!";
+const computerWins = "Computer wins! You lose.";
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "DRAW";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") return computerWins;
+    if (computerSelection === "scissors") return playerWins;
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") return computerWins;
+    if (computerSelection === "rock") return playerWins;
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") return computerWins;
+    if (computerSelection === "paper") return playerWins;
   }
-
-  function playRound(playerSelection, getComputerChoice) {}
 }
