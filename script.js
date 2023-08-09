8a45a70fcea1f655271a3ce6382ad792a3fa1a41
Shpat Spahiu <137379379+shpatspahiu@users.noreply.github.com>
2023-08-09 11:39:50 +0200

    Create more info on page
    Have problems checking the score, even though score updates, somwhere
    in the logic I have failed to check it and update the score on screen
    and/or end the game.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSOR = "Scissors";
 
+var playerScore = 0;
+var computerScore = 0;
+
 function getComputerChoice() {
   let max = 2;
   let min = 0;
@@ -21,25 +24,63 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  checkWinner();
   const playerWins = `You win! ${playerSelection} beats ${computerSelection}`;
   const computerWins = `You lose. ${computerSelection} beats ${playerSelection}`;
 
   if (playerSelection === computerSelection) {
     return "DRAW";
   } else if (playerSelection === ROCK) {
-    if (computerSelection === PAPER) return computerWins;
-    if (computerSelection === SCISSOR) return playerWins;
+    if (computerSelection === PAPER) {
+      computerScore++;
+      console.log(`computer score: ${computerScore}`);
+      return computerWins;
+    }
+    if (computerSelection === SCISSOR) {
+      playerScore++;
+      console.log(`player score: ${playerScore}`);
+      return playerWins;
+    }
   } else if (playerSelection === PAPER) {
-    if (computerSelection === SCISSOR) return computerWins;
-    if (computerSelection === ROCK) return playerWins;
+    if (computerSelection === SCISSOR) {
+      computerScore++;
+      console.log(`computer score: ${computerScore}`);
+      return computerWins;
+    }
+    if (computerSelection === ROCK) {
+      playerScore++;
+      console.log(`player score: ${playerScore}`);
+      return playerWins;
+    }
   } else if (playerSelection === SCISSOR) {
-    if (computerSelection === ROCK) return computerWins;
-    if (computerSelection === PAPER) return playerWins;
+    if (computerSelection === ROCK) {
+      computerScore++;
+      console.log(`computer score: ${computerScore}`);
+      return computerWins;
+    }
+    if (computerSelection === PAPER) {
+      playerScore++;
+      console.log(`player score: ${playerScore}`);
+      return playerWins;
+    }
   }
 }
 
-const resultHeading = document.querySelector("#roundResult h3");
+function checkWinner() {
+  if (playerScore >= 5 || computerScore >= 5) {
+    if (playerScore > computerScore) {
+      resultHeading.textContent = "Player Wins! Game Over.";
+    } else {
+      resultHeading.textContent = "Computer Wins! Game Over.";
+    }
+  }
+}
+
+const resultHeading = document.querySelector("#roundResult h2");
 resultHeading.textContent = "ROCK - PAPER - SCISSORS BABBOOOO";
+
+const score = document.querySelector("#roundResult p");
+score.textContent = `You: ${playerScore} | Computer: ${computerScore}`;
 
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
